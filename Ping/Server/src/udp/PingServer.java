40cba0719523fe00f90acf713071deda0bb5fbcd
Tahsin Al Sayeed <sayeed0635@gmail.com>
2017-10-07 17:07:04 +0600

    First assignment

---
+++ Ping/Server/src/udp/PingServer.java
@@ -0,0 +1,47 @@
+package udp;
+
+import java.io.*;
+import java.net.*;
+import java.util.Random;
+
+
+public class PingServer {
+
+    public static void main(String[] args) throws Exception
+    {
+        int port = 8080;
+
+        DatagramSocket socket = new DatagramSocket(port);
+
+        while (true) {
+            DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
+            socket.receive(request);
+            printData(request);
+            //simulate delay
+            Thread.sleep( (100));
+
+            InetAddress clientHost = request.getAddress();
+            int clientPort = request.getPort();
+            byte[] buf = request.getData();
+            DatagramPacket reply = new DatagramPacket(buf, buf.length, clientHost, clientPort);
+            socket.send(reply);
+
+        }
+    }
+
+    /*
+     * Print ping data to the standard output stream.
+     */
+    private static void printData(DatagramPacket request) throws Exception
+    {
+        byte[] buf = request.getData();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(buf)));
+
+        System.out.println(
+                "Received from " +
+                        request.getAddress().getHostAddress() +
+                        ": " +
+                        br.readLine() );
+    }
+}
